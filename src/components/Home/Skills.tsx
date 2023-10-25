45037eae1e7538bb354e26e4455c897ea344e705
palcisto <patrickalcisto@gmail.com>
2023-10-25 00:25:49 -0400

    added more skills

--- src/components/Home/Skills.tsx
+++ src/components/Home/Skills.tsx
@@ -4,42 +4,72 @@ const skills = [
   {
     skill: 'React',
     years: 8,
+    category: 'technical',
   },
   {
     skill: 'JavaScript',
     years: 10,
+    category: 'technical',
   },
   {
     skill: 'HTML & CSS',
     years: 19,
+    category: 'technical',
   },
   {
     skill: 'SCSS',
     years: 15,
+    category: 'technical',
   },
   {
     skill: 'TypeScript',
     years: 3,
+    category: 'technical',
   },
   {
     skill: 'GraphQL',
     years: 3,
+    category: 'technical',
   },
   {
     skill: 'Apollo',
     years: 2,
+    category: 'technical',
   },
   {
     skill: 'Redux',
     years: 1,
+    category: 'technical',
   },
   {
     skill: 'Photoshop',
     years: 10,
+    category: 'technical',
   },
   {
     skill: 'tailwindcss',
     years: 1,
+    category: 'technical',
+  },
+  {
+    skill: 'UX',
+    years: 12,
+    category: 'soft',
+  },
+  {
+    skill: 'Accessibility',
+    years: 12,
+    category: 'soft',
+  },
+  {
+    skill: 'Responsive Design',
+    years: 10,
+    category: 'soft',
+  },
+  {
+    skill: 'GIT',
+    years: 13,
+    category: 'technical',
   },
 ];
 
