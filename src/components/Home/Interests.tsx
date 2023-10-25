8c2f010c9cc962aff158df9b268a6686bc10d6cc
palcisto <patrickalcisto@gmail.com>
2023-10-25 00:16:47 -0400

    Added Interests section

---
+++ src/components/Home/Interests.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+export interface InterestsProps {}
+
+export default function Interests(props: InterestsProps) {
+  return (
+    <section>
+      <h3>Interests</h3>
+      <ul>
+        <li>Spending time w/ my girlfriend and dogs</li>
+        <li>Mountain biking</li>
+        <li>R/C Cars</li>
+        <li>Home renovation</li>
+      </ul>
+    </section>
+  );
+}
