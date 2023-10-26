2dfa32c27705767ccf9d38a0f7987924196073e4
palcisto <patrickalcisto@gmail.com>
2023-10-26 17:43:58 -0400

    move contact section to it's own component

---
+++ src/components/Home/Contact.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+export interface ContactProps {}
+
+export default function Contact(props: ContactProps) {
+  return (
+    <section id="contact">
+      <dl>
+        <dt>Email</dt>
+        <dd>
+          <a href="mailto:patrickalcisto@gmail.com">patrickalcisto@gmail.com</a>
+        </dd>
+      </dl>
+      <dl>
+        <dt>Phone</dt>
+        <dd>
+          <a href="tel:+16193487906">+1 (619) 348-7906</a>
+        </dd>
+      </dl>
+    </section>
+  );
+}
