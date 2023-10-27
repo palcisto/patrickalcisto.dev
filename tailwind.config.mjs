4d39c15c13ca9b3ec8cec374c012b91aafb91080
palcisto <patrickalcisto@gmail.com>
2023-10-27 17:53:40 -0400

    standardize theme colors and variables

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'hsl(var(--primary-color-base) / <alpha-value>)',
+        accent: 'hsl(var(--accent-color-base) / <alpha-value>)',
+        text: 'hsl(var(--text-color-base) / <alpha-value>)',
+        title: 'hsl(var(--title-color-base) / <alpha-value>)',
+      },
+    },
+  },
+  plugins: [],
+};
