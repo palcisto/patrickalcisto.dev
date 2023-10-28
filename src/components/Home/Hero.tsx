b6e113953d4b15e58958dd6868d0f0be71accffd
palcisto <patrickalcisto@gmail.com>
2023-10-27 20:43:47 -0400

    more styling

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -11,18 +11,16 @@ export interface HeroProps {}
 export default function Hero(props: HeroProps) {
   return (
     <section id="hero">
-      <div>
-        <h1 className="text-center md:text-left">
-          Senior <span>Frontend</span> Developer
-        </h1>
-        <h2>
-          Passionate about crafting excellent user experiences on the web and
-          mobile
-        </h2>
-        <a href="#" className="cta uppercase tracking-wide font-normal">
-          <span className="cta__inner">Hire me</span>
-        </a>
-      </div>
+      <h1 className="text-center md:text-left">
+        Senior <span>Frontend</span> Developer
+      </h1>
+      <h2>
+        Passionate about crafting excellent user experiences on the web and
+        mobile
+      </h2>
+      <a href="#" className="cta uppercase tracking-wide font-normal">
+        <span className="cta__inner">Hire me</span>
+      </a>
       <img className="hero__image" src="" alt="hero image" />
     </section>
   );
