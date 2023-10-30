9649320f860c17fde19ca39228b68443c594ef98
palcisto <patrickalcisto@gmail.com>
2023-10-30 10:11:15 -0400

    begin adding icons for skills

--- src/components/Home/Skills.tsx
+++ src/components/Home/Skills.tsx
@@ -1,75 +1,100 @@
 import * as React from 'react';
+import ReactIcon from '../icons/React';
+import NodeIcon from '../icons/Node';
+import JavascriptIcon from '../icons/Javascript';
+import TypeScriptIcon from '../icons/TypeScriptIcon';
+import GraphQLIcon from '../icons/GraphQLIcon';
 
 const skills = [
   {
     skill: 'React',
     years: 8,
     category: 'technical',
+    icon: ReactIcon,
   },
   {
     skill: 'JavaScript',
     years: 10,
     category: 'technical',
+    icon: JavascriptIcon,
   },
   {
     skill: 'HTML & CSS',
     years: 19,
     category: 'technical',
+    icon: () => null,
   },
   {
     skill: 'SCSS',
     years: 15,
     category: 'technical',
+    icon: () => null,
   },
   {
     skill: 'TypeScript',
     years: 3,
     category: 'technical',
+    icon: TypeScriptIcon,
+  },
+  {
+    skill: 'Node.js',
+    years: 2,
+    category: 'technical',
+    icon: NodeIcon,
   },
   {
     skill: 'GraphQL',
     years: 3,
     category: 'technical',
+    icon: GraphQLIcon,
   },
   {
     skill: 'Apollo',
     years: 2,
     category: 'technical',
+    icon: () => null,
   },
   {
     skill: 'Redux',
     years: 1,
     category: 'technical',
+    icon: () => null,
   },
   {
     skill: 'Photoshop',
     years: 10,
     category: 'technical',
+    icon: () => null,
   },
   {
     skill: 'tailwindcss',
     years: 1,
     category: 'technical',
+    icon: () => null,
   },
   {
     skill: 'UX',
     years: 12,
     category: 'soft',
+    icon: () => null,
   },
   {
     skill: 'Accessibility',
     years: 12,
     category: 'soft',
+    icon: () => null,
   },
   {
     skill: 'Responsive Design',
     years: 10,
     category: 'soft',
+    icon: () => null,
   },
   {
     skill: 'GIT',
     years: 13,
     category: 'technical',
+    icon: () => null,
   },
 ];
 
@@ -80,9 +105,9 @@ export default function Skills(props: SkillsProps) {
     <section id="skills">
       <h2>Skills</h2>
       <ul>
-        {skills.map(({ skill, years }) => (
-          <li key={skill}>
-            {skill} <span>{years}</span>
+        {skills.map(({ skill, icon: Icon }) => (
+          <li key={skill} className="flex gap-2 items-center">
+            <Icon /> {skill}
           </li>
         ))}
       </ul>
