d66f2a350f4dc6bf4b3a93f2b6c67ac29a30aa01
palcisto <patrickalcisto@gmail.com>
2024-01-10 18:34:29 -0500

    Completed footer styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.patrickalcisto.dev',
   integrations: [react(), tailwind()],
 });
